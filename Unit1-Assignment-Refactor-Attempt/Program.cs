4c1b893ce731a82c432631e419ec2ea91cbb428e
abailey76 <abailey76@duck.com>
2021-11-18 13:38:22 -0600

    trying to work through the challenge that was set forth

---
+++ Unit1-Assignment-Refactor-Attempt/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unit1AssignmentRefactorAttempt
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Welcome, " + userName + ". It's quiz time. Let's go!!\n");
+
+            List<string> questionList = new List<string>
+            {
+                "Who was the first American woman in space?",
+                "True or False: 5 kilometer == 5000 meters?",
+                "(5+3)/2*10 = ?",
+                "Given the array [8, 'Orbit', 'Trajectory, 45], what entry is at index 2?",
+                "What is the minimum crew size for the ISS?"
+            };
+
+            List<string> quizAnswers = new List<string>
+            {
+                "Sally Ride",
+                "true",
+                "40",
+                "Trajectory",
+                "3"
+            };
+
+            List<string> userAnswers = new List<string>
+            {
+
+            };
+
+            for (int i = 0; i < questionList.Count; i++)
+            {
+                Console.WriteLine("Question #{i}:" + questionList[i]);
+               
+            }
+
+        }
+    }
+}
